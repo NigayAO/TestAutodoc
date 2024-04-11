871ece9426c02ab8927ea5aa089603b0673d6cc9
Alik <Nigay_AO@bk.ru>
2024-04-12 00:43:24 +0300

    Initial Commit

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Alik Nigay on 12.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
